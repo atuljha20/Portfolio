1ebcc9a97c0109ea9ec231277c2fbe8f093cbc5e
Atul <atulkumaejha203@gmail.com>
2024-02-28 21:06:13 +0530

    initial commit

--- index2.js
+++ index2.js
@@ -1,7 +1,8 @@
- var typed= new typed(".text", {
-    Strings:["Frontend Devloper" , "Youtuber" , "Web Developer"],
+var typed = new TypedTextElement({".text",
+    strings: ["Frontend Developer", "Youtuber", "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
-    loop:true
-});
+    loop: true
+  });
+  
